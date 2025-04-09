852b4b84784cf4747d1bf29bae3999a9484c3349
Petro Kvartsianyi <kvartsaniy@gmail.com>
2025-04-09 19:31:55 +0200

    fix: remove .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/petrokvartsianyi/code/geomundus-website/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
