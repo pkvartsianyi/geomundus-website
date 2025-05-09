8f6319546469f0175df255ea4c90bd82687a5469
Petro Kvartsianyi <kvartsaniy@gmail.com>
2025-05-09 15:55:41 +0200

    refactor: docker ci

--- components/PostHogProvider.tsx
+++ components/PostHogProvider.tsx
@@ -9,7 +9,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "/ingest",
-      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
       capture_pageview: false,
       capture_pageleave: true,
       debug: process.env.NODE_ENV === "development",
--- lib/posthog.ts
+++ lib/posthog.ts
@@ -2,7 +2,7 @@ import { PostHog } from "posthog-node";
 
 export default function PostHogClient() {
   const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
     flushAt: 1,
     flushInterval: 0,
   });
